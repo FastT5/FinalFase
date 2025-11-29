8dfaede69da17808398a2b9a81f4fd237bbf3992
FastT5 <fastthunder3@gmail.com>
2025-11-29 13:23:06 -0600

    Agregar archivos HTML y CSS para la estructura y estilo del sitio web DentaLife

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function mostrarRegistro() {
+    document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('registroForm').style.display = 'block';
+}
+
+function mostrarLogin() {
+    document.getElementById('registroForm').style.display = 'none';
+    document.getElementById('loginForm').style.display = 'block';
+}
